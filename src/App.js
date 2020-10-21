dcd890dfd6cc962e612b2764c88fa685dab2d8a1
Will Lorand <will@milemarker3.org>
2020-10-21 13:43:35 -0700

    added styled components, google font, cra cleanup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+// child components
+import SocialCard from './components/SocialCard';
+// styles
+import './app.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        <h1>Styled Components Starter</h1>
       </header>
+      <SocialCard />
     </div>
   );
 }
---
+++ src/components/SocialCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// styled components
+import styled from 'styled-components';
+
+function SocialCard() {
+  return (
+    <div>
+      <h3>I am social card</h3>
+    </div>
+  );
+}
+
+export default SocialCard;
