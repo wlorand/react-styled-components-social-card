074a5b653198926e364d9efa8307afb64aeaa5ba
Will Lorand <will@milemarker3.org>
2020-10-21 14:49:48 -0700

    added styled components with vars and theme stub

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
 
+// styled components
+import { ThemeProvider } from 'styled-components';
+
 // child components
 import SocialCard from './components/SocialCard';
 // styles
 import './app.css';
 
+// styled components theme
+const redTheme = {
+  primary: 'red',
+};
+
 function App() {
   return (
     <div className="app">
-      <header className="app-header">
-        <h1>Styled Components Starter</h1>
-      </header>
-      <SocialCard />
+      <ThemeProvider theme={redTheme}>
+        <header className="app-header">
+          <h1>Styled Components Starter</h1>
+        </header>
+        <SocialCard />
+      </ThemeProvider>
     </div>
   );
 }
--- src/components/SocialCard.js
+++ src/components/SocialCard.js
@@ -3,10 +3,37 @@ import React from 'react';
 // styled components
 import styled from 'styled-components';
 
+// assets
+import Image from './images/guitar.jpg';
+
+// style vars
+// const basicBorder = '1px solid rgba(0, 0, 0.1)';
+
+const Card = styled.div`
+  border: 1px dotted ${(props) => props.theme.primary};
+  border-radius: 10px;
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.3);
+  margin: 30px auto;
+  max-width: 350px;
+  overflow: hidden;
+  /* nested styles supported */
+  img {
+    max-width: 100%;
+  }
+  /* media queries inline supported */
+  @media (max-width: 770px) {
+    background-color: rgba(189, 0, 0, 0.33);
+  }
+`;
+
 function SocialCard() {
   return (
     <div>
       <h3>I am social card</h3>
+      <Card>
+        <img src={Image} alt="EVH Guitar" />
+        <p>Inside the card</p>
+      </Card>
     </div>
   );
 }
